111a3fb6c9aaa8e853e86a4d90b100d5b038d45f
MaksimBaturin <baturbaya@yandex.ru>
2024-03-21 21:59:41 +0300

    Add trigger to change camera physics on enter and exit of battle arena

---
+++ scenes/game/Utils/TriggerOnEnterBattle.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public partial class TriggerOnEnterBattle : Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void _on_trigger_area_body_exited(Node2D body){
+		if (body.Name == "Player"){
+			GetParent().GetParent().GetNode<PlatformingBorder>("PlayerCamera/PlatformingBorder").turn_off();
+			GetParent().GetParent().GetNode<in_level_ui>("InLevelUI").animate_out();
+			GetParent().GetParent().Set("isMoveableCamera", true);
+		}
+	}
+}
