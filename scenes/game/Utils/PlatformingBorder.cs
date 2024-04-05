92365877c53727f1624411d09633dba0758b4df4
MaksimBaturin <baturbaya@yandex.ru>
2024-04-05 22:11:24 +0300

    Fixed distance beetwen border and player after teleportation

--- scenes/game/Utils/PlatformingBorder.cs
+++ scenes/game/Utils/PlatformingBorder.cs
@@ -15,13 +15,13 @@ public partial class PlatformingBorder : Node2D
 	
 	public void _on_area_left_body_entered(Node2D body){
 		if (body.Name == "Player"){
-			body.CallDeferred("teleport_to", GetNode<Area2D>("AreaRight").Position.X-20);
+			body.CallDeferred("teleport_to", GetNode<Area2D>("AreaRight").Position.X-30);
 		}
 		
 	}
 	public void _on_area_right_body_entered(Node2D body){
 		if (body.Name == "Player"){
-			body.CallDeferred("teleport_to", GetNode<Area2D>("AreaLeft").Position.X+20);
+			body.CallDeferred("teleport_to", GetNode<Area2D>("AreaLeft").Position.X+30);
 		}
 	}
 
