89b6937fb821f82a81b59ccd27496495ebc3b489
MaksimBaturin <baturbaya@yandex.ru>
2024-04-14 17:08:21 +0300

    Add level 3

--- scenes/game/Level/Level3/LevelGeneratorLevel3.cs
+++ scenes/game/Level/Level3/LevelGeneratorLevel3.cs
@@ -1,23 +1,23 @@
 using Godot;
 using System;
 
-public partial class LevelGeneratorLevel2 : LevelGenerator
+public partial class LevelGeneratorLevel3 : LevelGenerator
 {
 	public override void _Ready()
 	{
 		BattleSection = new Godot.Collections.Array<PackedScene>{
-		GD.Load<PackedScene>("res://scenes/game/LevelItems/Rooms/Level2/Room1.tscn")
+		GD.Load<PackedScene>("res://scenes/game/LevelItems/Rooms/Level3/Room1.tscn")
 		};
 
 		EndRooms = new Godot.Collections.Array<PackedScene>{
-		GD.Load<PackedScene>("res://scenes/game/LevelItems/Rooms/Level2/EndRooms/EndRoom1.tscn")
+		GD.Load<PackedScene>("res://scenes/game/LevelItems/Rooms/Level3/EndRooms/EndRoom.tscn")
 		};
 
 		Enemies = new Godot.Collections.Array<PackedScene>{
 		GD.Load<PackedScene>("res://scenes/game/entities/FloatingEye/FloatingEye.tscn")
 		};
 
-		NextScenePath = "res://scenes/game/Level/Level3/level3.tscn";
+		NextScenePath = "res://scenes/Menu/menu.tscn";
 
 		Player = GetParent().GetNode<player>("Player");
 	
