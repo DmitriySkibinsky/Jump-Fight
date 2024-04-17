44d9f80f74243371dd8eac1b14bac0dbdbded7d5
Dmitriy <70383822+DmitriySkiba@users.noreply.github.com>
2024-04-17 18:23:05 +0300

    Update CustomSignals.cs

--- global/CustomSignals.cs
+++ global/CustomSignals.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class CustomSignals : Node
+public partial class CustomSignals : Node2D
 {
 	[Signal]
 	public delegate void DamagePlayerEventHandler(int Damage);
