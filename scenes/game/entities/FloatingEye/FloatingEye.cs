5d7e01990306d8f143b4a1b0cb78c1280ac8642f
AsanSmailov <asan.smailov.06@mail.ru>
2024-04-21 19:39:06 +0300

    Add tests

--- scenes/game/entities/FloatingEye/FloatingEye.cs
+++ scenes/game/entities/FloatingEye/FloatingEye.cs
@@ -143,20 +143,20 @@ public static class Trajectory
 }
 public partial class FloatingEye : CharacterBody2D
 {
-    public static int Speed = 125;
-    public static int Damage = 20;
-    public static int direction = 1;
-    public static bool Alive = true;
+    public int Speed = 125;
+    public int Damage = 20;
+    public int direction = 1;
+    public bool Alive = true;
 
-    public static Vector2[] WayPoints; // Путь/Траектория по которому движеться враг
+    public  Vector2[] WayPoints; // Путь/Траектория по которому движеться враг
     private Vector2 StartPos; // Стартовая позиция
-    public static int CurrentWayPoint; // Указывает на индекс Вейпоинта к которому он движется
+    public  int CurrentWayPoint; // Указывает на индекс Вейпоинта к которому он движется
 
-    public static AnimatedSprite2D Anim;
+    public  AnimatedSprite2D Anim;
     private Area2D HitBoxes;
     private Area2D HurtBoxes;
 
-    public static player Player;
+    public player Player;
 
 
     public override void _Ready()
